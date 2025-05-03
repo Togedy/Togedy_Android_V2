62b1d6959a0ede7548c50a8cd12f357b1a574560
chrin05 <rinring105@gmail.com>
2025-05-04 00:47:35 +0900

    [mod/#1] build-logic: gradle, settings 수정

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "com.napzak.market.buildlogic"
+group = "com.together.study.buildlogic"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -10,7 +10,6 @@ java {
 }
 
 dependencies {
-    testImplementation(libs.junit.junit)
     compileOnly(libs.android.gradle)
     compileOnly(libs.kotlin.gradle)
     compileOnly(libs.compose.compiler.gradle)
@@ -18,6 +17,54 @@ dependencies {
 
 gradlePlugin {
     plugins {
+        register("com.together.study.buildlogic.primitive.CommonAndroidPlugin") {
+            id = "com.together.study.buildlogic.primitive.common"
+            implementationClass = "com.together.study.buildlogic.primitive.CommonAndroidPlugin"
+        }
 
+        register("com.together.study.buildlogic.primitive.ComposePlugin") {
+            id = "com.together.study.buildlogic.primitive.compose"
+            implementationClass = "com.together.study.buildlogic.primitive.ComposePlugin"
+        }
+
+        register("com.together.study.buildlogic.primitive.HiltPlugin") {
+            id = "com.together.study.buildlogic.primitive.hilt"
+            implementationClass = "com.together.study.buildlogic.primitive.HiltPlugin"
+        }
+
+        register("com.together.study.buildlogic.primitive.KotlinPlugin") {
+            id = "com.together.study.buildlogic.primitive.kotlin"
+            implementationClass = "com.together.study.buildlogic.primitive.KotlinPlugin"
+        }
+
+        register("com.together.study.buildlogic.primitive.RetrofitPlugin") {
+            id = "com.together.study.buildlogic.primitive.retrofit"
+            implementationClass = "com.together.study.buildlogic.primitive.RetrofitPlugin"
+        }
+
+        register("com.together.study.buildlogic.primitive.OkHttpPlugin") {
+            id = "com.together.study.buildlogic.primitive.okhttp"
+            implementationClass = "com.together.study.buildlogic.primitive.OkHttpPlugin"
+        }
+
+        register("com.together.study.buildlogic.primitive.TestPlugin") {
+            id = "com.together.study.buildlogic.primitive.test"
+            implementationClass = "com.together.study.buildlogic.primitive.TestPlugin"
+        }
+
+        register("com.together.study.buildlogic.convention.ApplicationPlugin") {
+            id = "com.together.study.buildlogic.convention.application"
+            implementationClass = "com.together.study.buildlogic.convention.ApplicationPlugin"
+        }
+
+        register("com.together.study.buildlogic.convention.ComposePlugin") {
+            id = "com.together.study.buildlogic.convention.compose"
+            implementationClass = "com.together.study.buildlogic.convention.ComposePlugin"
+        }
+
+        register("com.together.study.buildlogic.convention.FeaturePlugin") {
+            id = "com.together.study.buildlogic.convention.feature"
+            implementationClass = "com.together.study.buildlogic.convention.FeaturePlugin"
+        }
     }
 }
