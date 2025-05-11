543b1b9079957603d4d40145897cc20cabb7c859
chrin05 <rinring105@gmail.com>
2025-05-11 21:45:57 +0900

    [fix/#1] presentation:dummy gradle 수정

--- presentation/dummy/build.gradle.kts
+++ presentation/dummy/build.gradle.kts
@@ -3,15 +3,21 @@ import com.together.study.buildlogic.dsl.setNameSpace
 plugins {
     id("com.together.study.buildlogic.convention.feature")
     id("com.together.study.buildlogic.primitive.hilt")
-    id("com.together.study.buildlogic.primitive.retrofit")
-    id("com.together.study.buildlogic.primitive.okhttp")
+    id("com.together.study.buildlogic.convention.compose")
 }
 
 android {
-    setNameSpace("data.dummy")
+    setNameSpace("presentation.dummy")
 }
 
 dependencies {
+
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+    implementation(projects.core.util)
+
     implementation(projects.domain.dummy)
-    implementation(libs.timber)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
 }
