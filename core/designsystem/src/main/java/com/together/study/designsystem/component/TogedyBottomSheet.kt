89084e997b19fa24aa8bfb177bccb9e9fa17a51c
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-05-18 14:01:24 +0900

    [ui/#8] togedy bottomsheet 구현

---
+++ core/designsystem/src/main/java/com/together/study/designsystem/component/TogedyBottomSheet.kt
@@ -0,0 +1,64 @@
+package com.together.study.designsystem.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.together.study.designsystem.theme.TogedyTheme
+import com.together.study.util.noRippleClickable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TogedyBottomSheet(
+    sheetState: SheetState,
+    onDismissRequest: () -> Unit,
+    title: String = "",
+    showDone: Boolean = false,
+    onDoneClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    ModalBottomSheet(
+        onDismissRequest = { onDismissRequest() },
+        sheetState = sheetState,
+        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        containerColor = TogedyTheme.colors.white,
+        dragHandle = null
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = title,
+                    modifier = Modifier.align(
+                        alignment = Alignment.Companion.Center
+                    )
+                )
+
+                if (showDone) {
+                    Text(
+                        text = "완료",
+                        modifier = Modifier
+                            .align(alignment = Alignment.Companion.CenterEnd)
+                            .noRippleClickable {
+                                onDoneClick()
+                            }
+                    )
+                }
+            }
+            content()
+        }
+    }
+}
