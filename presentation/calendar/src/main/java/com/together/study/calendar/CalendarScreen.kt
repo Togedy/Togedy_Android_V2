1c7e02980f1d4ededfd870cb2f7b6a0e2b56e6e3
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-05-18 16:03:13 +0900

    [mod/#8] calednar screen 더미 구현

--- presentation/calendar/src/main/java/com/together/study/calendar/CalendarScreen.kt
+++ presentation/calendar/src/main/java/com/together/study/calendar/CalendarScreen.kt
@@ -1,10 +1,26 @@
 package com.together.study.calendar
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.together.study.designsystem.component.TogedySearchBar
 
 @Composable
 fun CalendarScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(top = 50.dp)
+    ) {
+        TogedySearchBar(
+            isShowSearch = true,
+        )
+    }
 }
 
 @Preview
