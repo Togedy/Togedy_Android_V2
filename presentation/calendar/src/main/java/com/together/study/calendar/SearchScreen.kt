b0f5b029035a186917beec79972cb95306cd3d4f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-05-18 13:42:19 +0900

    [add/#8] presentation:calendar calendar, search 스크린 추가

---
+++ presentation/calendar/src/main/java/com/together/study/calendar/SearchScreen.kt
@@ -0,0 +1,14 @@
+package com.together.study.calendar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchScreen() {
+}
+
+@Preview
+@Composable
+fun SearchScreenPreview(){
+    SearchScreen()
+}
