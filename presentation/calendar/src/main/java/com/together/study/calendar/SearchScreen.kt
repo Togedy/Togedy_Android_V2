15867e7defe40e13318e2bfb7b57ab75cb402a9c
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-06-08 21:54:35 +0900

    [ui/#8] search screen 드롭다운 및 로직 구현

--- presentation/calendar/src/main/java/com/together/study/calendar/SearchScreen.kt
+++ presentation/calendar/src/main/java/com/together/study/calendar/SearchScreen.kt
@@ -1,19 +1,28 @@
 package com.together.study.calendar
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -22,30 +31,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.together.study.calendar.component.SearchSelectorChip
+import com.together.study.calendar.component.SearchSelectorHeader
 import com.together.study.designsystem.component.TogedyBottomSheet
 import com.together.study.designsystem.component.TogedySearchBar
 import com.together.study.designsystem.R
 import com.together.study.designsystem.component.TogedyScheduleChip
 import com.together.study.designsystem.theme.TogedyTheme
+import com.together.study.util.noRippleClickable
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchScreen() {
-    var searchValue by remember { mutableStateOf("") }
+fun SearchScreen(
+    viewModel: SearchViewModel = viewModel()
 
-    val allList = remember { dummyScheduleList() }
-    var filteredList by remember {
-        mutableStateOf(
-            allList.sortedByDescending { it.isAdded }
-        )
-    }
+) {
+    val searchValue by viewModel.searchQuery.collectAsStateWithLifecycle()
+    val filteredList by viewModel.filteredList.collectAsStateWithLifecycle()
 
     val coroutineScope = rememberCoroutineScope()
-    var searchJob by remember { mutableStateOf<Job?>(null) }
 
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     var selectedData by remember { mutableStateOf<SearchScheduleData?>(null) }
@@ -82,19 +91,7 @@ fun SearchScreen() {
             )
             TogedySearchBar(
                 value = searchValue,
-                onValueChange = { it ->
-                    searchValue = it
-
-                    searchJob?.cancel()
-                    searchJob = coroutineScope.launch {
-                        delay(300)
-                        filteredList = allList
-                            .filter { item ->
-                                item.universityName.contains(searchValue, ignoreCase = true)
-                            }
-                            .sortedByDescending { it.isAdded }
-                    }
-                }
+                onValueChange = { viewModel.onSearchQueryChanged(it) }
             )
         }
 
@@ -123,7 +120,7 @@ fun SearchScreen() {
                 onCloseBottomSheet()
             },
             title = "대학일정",
-            showDone = true,
+            showDone = false,
             onDoneClick = {
                 onCloseBottomSheet()
             }
@@ -139,28 +136,92 @@ fun SearchScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheetContent(data: SearchScheduleData?) {
-    Column(
+    if (data == null) return
 
+    val admissionList = data.admissionList
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    val selectedAdmission = admissionList.getOrNull(selectedIndex)
+
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
+        SearchSelectorHeader(
+            admissionType = data.admissionType,
+            universityName = data.universityName,
+            isAdded = data.isAdded
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded }
+        ) {
+            Box(
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .noRippleClickable {
+                        isExpanded = true
+                    }
+                    .border(
+                        width = 1.dp,
+                        color = TogedyTheme.colors.gray300,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(vertical = 8.dp, horizontal = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = selectedAdmission?.admissionMethod ?: "전형 선택",
+                        style = TogedyTheme.typography.body14m.copy(
+                            color = TogedyTheme.colors.gray300
+                        ),
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Icon(
+                        painter = painterResource(R.drawable.ic_drop_down_24),
+                        contentDescription = "드롭다운 이미지"
+                    )
+                }
+            }
+
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = {
+                    isExpanded = false
+                },
+            ) {
+                admissionList.forEachIndexed { index, admission ->
+                    DropdownMenuItem(
+                        text = { Text(admission.admissionMethod) },
+                        onClick = {
+                            selectedIndex = index
+                            isExpanded = false
+                        },
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
 
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .padding(top = 8.dp, bottom = 25.dp),
+                .padding(bottom = 25.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            data?.admissionList?.forEach { admission ->
-                item {
-                    Text(
-                        text = admission.admissionMethod,
-                        style = TogedyTheme.typography.body14m,
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
-                }
-
-                items(admission.universityScheduleList.size) { index ->
-                    val schedule = admission.universityScheduleList[index]
+            selectedAdmission?.universityScheduleList?.let { scheduleList ->
+                items(scheduleList.size) { index ->
+                    val schedule = scheduleList[index]
                     TogedyScheduleChip(
                         typeStatus = when (schedule.admissionStage) {
                             "원서접수" -> 1
@@ -169,7 +230,7 @@ fun BottomSheetContent(data: SearchScheduleData?) {
                             else -> 0
                         },
                         scheduleName = data.universityName,
-                        scheduleType = admission.admissionMethod,
+                        scheduleType = selectedAdmission.admissionMethod,
                         scheduleStartTime = schedule.startDate,
                         scheduleEndTime = schedule.endDate
                     )
