44d099dd0101c4cdd5ef288ad059da4de905a566
chrin05 <rinring105@gmail.com>
2025-07-12 20:46:38 +0900

    [feat/#18] data > mapper 구현

---
+++ data/calendar/src/main/java/com/together/study/calendar/mapper/UserScheduleMapper.kt
@@ -0,0 +1,29 @@
+package com.together.study.calendar.mapper
+
+import com.together.study.calendar.dto.UserScheduleRequest
+import com.together.study.calendar.dto.UserScheduleResponse
+import com.together.study.calendar.model.UserSchedule
+
+fun UserSchedule.toData(): UserScheduleRequest =
+    UserScheduleRequest(
+        userScheduleName = userScheduleName,
+        startDate = startDate,
+        startTime = startTime,
+        endDate = endDate,
+        endTime = endTime,
+        categoryId = categoryId,
+        memo = memo,
+        dDay = dDay,
+    )
+
+fun UserScheduleResponse.toDomain(): UserSchedule =
+    UserSchedule(
+        userScheduleName = userScheduleName,
+        startDate = startDate,
+        startTime = startTime,
+        endDate = endDate,
+        endTime = endTime,
+        categoryId = categoryId,
+        memo = memo,
+        dDay = dDay,
+    )
