822c7ad7cab1df85106bcbba35c26afeee8377ef
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-07-23 00:16:54 +0900

    [ui/#12] 입시 필터 영역 구현

--- presentation/search/src/main/java/com/together/study/search/component/SearchSelectorAdmissionType.kt
+++ presentation/search/src/main/java/com/together/study/search/component/SearchSelectorAdmissionType.kt
@@ -1,6 +1,7 @@
 package com.together.study.search.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import com.together.study.designsystem.theme.TogedyTheme
 import com.together.study.search.type.AdmissionType
@@ -32,15 +34,20 @@ fun SearchSelectorAdmissionType(
             )
             .padding(vertical = 4.dp, horizontal = 6.dp),
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(2.dp)
     ) {
         items.forEach { type ->
             val isSelected = selectedType == type
             Box(
                 modifier = Modifier
                     .weight(1f)
+                    .shadow(
+                        elevation = if (isSelected) 4.dp else 0.dp,
+                        shape = RoundedCornerShape(8.dp),
+                    )
                     .background(
                         color = if (isSelected) TogedyTheme.colors.white else TogedyTheme.colors.gray100,
-                        shape = RoundedCornerShape(8.dp)
+                        shape = RoundedCornerShape(8.dp),
                     )
                     .padding(vertical = 3.dp)
                     .noRippleClickable { onSelect(type) },
@@ -48,7 +55,9 @@ fun SearchSelectorAdmissionType(
             ) {
                 Text(
                     text = type.admissionName,
-                    color = if (isSelected) TogedyTheme.colors.gray700 else TogedyTheme.colors.gray600
+                    style = TogedyTheme.typography.title16sb.copy(
+                        color = if (isSelected) TogedyTheme.colors.gray800 else TogedyTheme.colors.gray600
+                    )
                 )
             }
         }
