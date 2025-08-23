b06b80c67d0e0fd7057296f54331b682d3fc21a7
TaejungKim <tae0803@naver.com>
2025-08-23 17:02:58 +0900

    [feat/#28] service 구현

--- data/search/src/main/java/com/together/study/search/service/UnivScheduleService.kt
+++ data/search/src/main/java/com/together/study/search/service/UnivScheduleService.kt
@@ -1,14 +1,16 @@
 package com.together.study.search.service
 
 import com.together.study.remote.model.BaseResponse
-import com.together.study.search.dto.UnivScheduleRequest
 import com.together.study.search.dto.UnivScheduleResponse
-import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface UnivScheduleService {
     @GET("calendars/universities")
-    suspend fun getUserSchedule(
-        @Body univScheduleRequest: UnivScheduleRequest,
+    suspend fun getUnivSchedule(
+        @Query("name") name: String,
+        @Query("admission-type") admissionType : String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
     ): BaseResponse<List<UnivScheduleResponse>>
 }
