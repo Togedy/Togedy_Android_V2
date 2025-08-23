88449639151b4e8f384e3effbc51ca6ab5319d02
TaejungKim <tae0803@naver.com>
2025-08-23 17:02:27 +0900

    [feat/#28] domain > repository 구현

--- domain/search/src/main/java/com/together/study/search/repository/UnivScheduleRepository.kt
+++ domain/search/src/main/java/com/together/study/search/repository/UnivScheduleRepository.kt
@@ -3,5 +3,10 @@ package com.together.study.search.repository
 import com.together.study.search.model.UnivSchedule
 
 interface UnivScheduleRepository {
-    suspend fun getUnivScheduleList(): Result<List<UnivSchedule>>
+    suspend fun getUnivScheduleList(
+        name: String,
+        admissionType: String,
+        page: Int,
+        size: Int
+    ): Result<List<UnivSchedule>>
 }
