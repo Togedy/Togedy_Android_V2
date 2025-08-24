5a392a66419191dc7acdc3559d98c7dd384cf24a
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-08-24 11:13:13 +0900

    [feat/#28] viewmodel api 연결 및 더미데이터 삭제

--- presentation/search/src/main/java/com/together/study/search/SearchDetailViewModel.kt
+++ presentation/search/src/main/java/com/together/study/search/SearchDetailViewModel.kt
@@ -1,68 +1,63 @@
 package com.together.study.search
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.together.study.common.state.UiState
+import com.together.study.search.model.AdmissionMethod
+import com.together.study.search.model.UnivDetailSchedule
+import com.together.study.search.repository.UnivScheduleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchDetailViewModel @Inject constructor() : ViewModel() {
+internal class SearchDetailViewModel @Inject constructor(
+    private val univScheduleRepository: UnivScheduleRepository
+) : ViewModel() {
 
-    private val _searchDummy = MutableStateFlow<SearchDetailDummy?>(null)
-    val searchDummy: StateFlow<SearchDetailDummy?> = _searchDummy
+    private val _univDetailScheduleState = MutableStateFlow<UiState<UnivDetailSchedule>>(UiState.Loading)
+    val univDetailScheduleState: StateFlow<UiState<UnivDetailSchedule>> = _univDetailScheduleState
+    
+    private var currentUniversityId: Int = 0
 
     fun loadUniversityDetail(universityId: Int) {
-        // TODO: API 연결 시 universityId로 실제 데이터를 가져오도록 수정
-        _searchDummy.value = getUniversityDetailById(universityId)
+        currentUniversityId = universityId
+        viewModelScope.launch {
+            _univDetailScheduleState.value = UiState.Loading
+            
+            univScheduleRepository.getUnivDetailSchedule(universityId)
+                .onSuccess { detailSchedule -> 
+                    _univDetailScheduleState.value = UiState.Success(detailSchedule)
+                }
+                .onFailure { exception -> 
+                    _univDetailScheduleState.value = UiState.Failure(exception.message.toString())
+                }
+        }
     }
 
     fun deleteAddedItem(admissionMethodId: Int) {
-        val currentData = _searchDummy.value ?: return
-
-        // 해당 admissionMethodId 삭제
-        val updatedAddedList = currentData.addedAdmissionMethodList.filter { addedItem ->
-            currentData.universityAdmissionMethodList.any { method ->
-                method.universityAdmissionMethod == addedItem && method.universityAdmissionMethodId != admissionMethodId
-            }
+        viewModelScope.launch {
+            univScheduleRepository.deleteUnivDetailSchedule(admissionMethodId)
+                .onSuccess { 
+                    loadUniversityDetail(currentUniversityId)
+                }
+                .onFailure { exception ->
+                }
         }
-
-        // TODO: API 연결 시 데이터 다시 가져오게 수정
-        val updatedData = currentData.copy(
-            addedAdmissionMethodList = updatedAddedList
-        )
-
-        _searchDummy.value = updatedData
     }
 
     fun addAdmissionMethod(admissionMethodId: Int) {
-        val currentData = _searchDummy.value ?: return
-        
-        // 해당 admissionMethodId를 가진 전형 찾기
-        val admissionMethod = currentData.universityAdmissionMethodList.find { 
-            it.universityAdmissionMethodId == admissionMethodId 
-        } ?: return
-        
-        // 이미 추가된 전형인지 확인
-        val isAlreadyAdded = currentData.addedAdmissionMethodList.contains(admissionMethod.universityAdmissionMethod)
-        if (isAlreadyAdded) return
-        
-        // 새로운 전형 추가
-        val updatedAddedList = currentData.addedAdmissionMethodList + admissionMethod.universityAdmissionMethod
-        
-        // TODO: API 연결 시 데이터 다시 가져오게 수정
-        val updatedData = currentData.copy(
-            addedAdmissionMethodList = updatedAddedList
-        )
-        
-        _searchDummy.value = updatedData
-    }
-
-    private fun getUniversityDetailById(universityId: Int): SearchDetailDummy {
-        // TODO: API 연결 시 실제 데이터베이스에서 가져오도록 수정
-        return when (universityId) {
-            1 -> universityData
-            else -> universityData
+        viewModelScope.launch {
+            val admissionMethod = AdmissionMethod(universityAdmissionMethodId = admissionMethodId)
+            univScheduleRepository.addUnivDetailSchedule(admissionMethod)
+                .onSuccess { 
+                    loadUniversityDetail(currentUniversityId)
+                }
+                .onFailure { exception ->
+                }
         }
     }
 }
