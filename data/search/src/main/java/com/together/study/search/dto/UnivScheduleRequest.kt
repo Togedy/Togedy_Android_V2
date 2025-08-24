42eb10705e4394d124546b81be7b739ab47152c2
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-08-24 11:11:04 +0900

    [feat/#28] 검색 대학 상세 일정 관련 dto 추가

--- data/search/src/main/java/com/together/study/search/dto/UnivScheduleRequest.kt
+++ data/search/src/main/java/com/together/study/search/dto/UnivScheduleRequest.kt
@@ -4,9 +4,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UnivScheduleRequest(
-    @SerialName("name") val name: String,
-    @SerialName("admission-type") val admissionType: String,
-    @SerialName("page") val page: Int,
-    @SerialName("size") val size: Int,
+data class UnivDetailScheduleAddRequest(
+    @SerialName("universityAdmissionMethodId") val universityAdmissionMethodId: Int,
 )
