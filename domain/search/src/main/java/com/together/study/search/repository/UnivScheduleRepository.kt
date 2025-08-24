e0ebccd783a3bde71a0a87b6a4d1eeaeb80b10be
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-08-24 11:09:47 +0900

    [feat/#28] domain > repository 구현

--- domain/search/src/main/java/com/together/study/search/repository/UnivScheduleRepository.kt
+++ domain/search/src/main/java/com/together/study/search/repository/UnivScheduleRepository.kt
@@ -1,5 +1,7 @@
 package com.together.study.search.repository
 
+import com.together.study.search.model.AdmissionMethod
+import com.together.study.search.model.UnivDetailSchedule
 import com.together.study.search.model.UnivSchedule
 
 interface UnivScheduleRepository {
@@ -9,4 +11,10 @@ interface UnivScheduleRepository {
         page: Int,
         size: Int
     ): Result<List<UnivSchedule>>
+    
+    suspend fun getUnivDetailSchedule(universityId: Int): Result<UnivDetailSchedule>
+    
+    suspend fun deleteUnivDetailSchedule(universityAdmissionMethodId: Int): Result<Unit>
+    
+    suspend fun addUnivDetailSchedule(request: AdmissionMethod): Result<Unit>
 }
