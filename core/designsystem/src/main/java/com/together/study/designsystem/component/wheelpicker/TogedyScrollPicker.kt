27d7aa51b2cd2b90095093d5cf46a83837a36db5
chrin05 <rinring105@gmail.com>
2025-08-31 23:03:14 +0900

    [mod/#26] 그라데이션 추가

--- core/designsystem/src/main/java/com/together/study/designsystem/component/wheelpicker/TogedyScrollPicker.kt
+++ core/designsystem/src/main/java/com/together/study/designsystem/component/wheelpicker/TogedyScrollPicker.kt
@@ -5,7 +5,10 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,6 +24,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -122,6 +127,38 @@ fun TogedyScrollPicker(
                     )
                 }
             }
+        }
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                TogedyTheme.colors.white,
+                                Color.Transparent,
+                            )
+                        )
+                    )
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                TogedyTheme.colors.white,
+                            )
+                        )
+                    )
+            )
         }
     }
 }
