f9c0e1d718f90d86a5e88b896096e97f8f433417
chrin05 <rinring105@gmail.com>
2025-09-27 14:26:08 +0900

    [add/#35] 더미 데이터 추가

--- presentation/study/src/main/java/com/together/study/study/main/state/StudyMainUiState.kt
+++ presentation/study/src/main/java/com/together/study/study/main/state/StudyMainUiState.kt
@@ -28,16 +28,42 @@ data class TimerInfo(
 /* TODO: domain으로 이동 예정 */
 data class Study(
     val studyType: String,
-    val challengeGoalTime: String,
-    val challengeAcheivement: Int,
+    val challengeGoalTime: String?,
+    val challengeAchievement: Int?,
     val studyName: String,
-    val completedMemberCount: Int,
+    val completedMemberCount: Int?,
     val studyMemberCount: Int,
     val activeMemberList: List<User>,
-)
+) {
+    companion object {
+        val mock1 = Study(
+            studyType = "CHALLENGE",
+            challengeGoalTime = "10:00:00",
+            challengeAchievement = 75,
+            studyName = "을지중학교 1-1",
+            completedMemberCount = 3,
+            studyMemberCount = 5,
+            activeMemberList = listOf(User.mock1, User.mock2)
+        )
+        val mock2 = Study(
+            studyType = "NORMAL",
+            challengeGoalTime = null,
+            challengeAchievement = null,
+            studyName = "고1 내신 대비반",
+            completedMemberCount = null,
+            studyMemberCount = 5,
+            activeMemberList = listOf(User.mock1, User.mock2, User.mock1, User.mock2, User.mock1)
+        )
+    }
+}
 
 /* TODO: domain으로 이동 예정 */
 data class User(
     val userName: String,
     val userProfileImageUrl: String,
-)
+) {
+    companion object {
+        val mock1 = User(userName = "토마토마토", userProfileImageUrl = "")
+        val mock2 = User(userName = "더블치즈감자", userProfileImageUrl = "")
+    }
+}
