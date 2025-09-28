1218db579e6b0ef035e0324622cefdd156d2c2b3
chrin05 <rinring105@gmail.com>
2025-09-28 17:58:10 +0900

    [mod/#35] tab 애니메이션 적용

--- presentation/study/src/main/java/com/together/study/study/main/component/MainTabSection.kt
+++ presentation/study/src/main/java/com/together/study/study/main/component/MainTabSection.kt
@@ -1,5 +1,7 @@
 package com.together.study.study.main.component
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,13 +9,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.together.study.designsystem.component.tabbar.StudyMainTab
 import com.together.study.designsystem.theme.TogedyTheme
@@ -62,16 +71,37 @@ private fun TabItem(
     backgroundColor: Color,
     onTabClick: () -> Unit,
 ) {
+    val density = LocalDensity.current
+
     val lineColor = if (isSelected) textColor else backgroundColor
+
+    var textWidth by remember { mutableStateOf(0.dp) }
+    val targetWidth: Dp = if (isSelected) textWidth + 12.dp else 0.dp
+
+    val animatedWidth: Dp by animateDpAsState(
+        targetValue = targetWidth,
+        animationSpec = tween(durationMillis = 250),
+        label = "UnderlineWidth"
+    )
 
     Column(
         modifier = Modifier.noRippleClickable(onTabClick),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
             text = tabName,
             style = TogedyTheme.typography.title16sb,
             color = textColor,
-            modifier = Modifier.padding(horizontal = 8.dp),
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .layout { measurable, constraints ->
+                    val placeable = measurable.measure(constraints)
+                    textWidth = with(density) { placeable.width.toDp() }
+
+                    layout(placeable.width, placeable.height) {
+                        placeable.placeRelative(0, 0)
+                    }
+                }
         )
 
         Spacer(Modifier.height(8.dp))
@@ -79,15 +109,8 @@ private fun TabItem(
         Box(
             modifier = Modifier
                 .height(1.5.dp)
-                .wrapContentWidth()
+                .width(animatedWidth)
                 .background(color = lineColor),
-        ) {
-            Text(
-                text = tabName,
-                style = TogedyTheme.typography.title16sb,
-                color = backgroundColor,
-                modifier = Modifier.padding(horizontal = 8.dp),
-            )
-        }
+        )
     }
 }
