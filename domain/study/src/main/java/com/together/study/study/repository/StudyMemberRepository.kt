ad71aef1c9d85a4f3a291c08d404bc0fd884c849
chrin05 <rinring105@gmail.com>
2025-10-25 20:23:06 +0900

    [feat/#44] 유저 프로필 Repository 구현

---
+++ domain/study/src/main/java/com/together/study/study/repository/StudyMemberRepository.kt
@@ -0,0 +1,11 @@
+package com.together.study.study.repository
+
+import com.together.study.study.model.StudyMemberPlanner
+import com.together.study.study.model.StudyMemberProfile
+import com.together.study.study.model.StudyMemberTimeBlocks
+
+interface StudyMemberRepository {
+    suspend fun getStudyMemberProfile(studyId: Long, userId: Long): Result<StudyMemberProfile>
+    suspend fun getStudyMemberTimeBlocks(studyId: Long, userId: Long): Result<StudyMemberTimeBlocks>
+    suspend fun getStudyMemberPlanner(studyId: Long, userId: Long): Result<StudyMemberPlanner>
+}
