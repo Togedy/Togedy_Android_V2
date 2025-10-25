c1c3f0065936b7ba3c903dff0e7de16f371588b3
chrin05 <rinring105@gmail.com>
2025-10-25 20:23:23 +0900

    [feat/#44] 유저 프로필 dto 추가 및 service 구현

---
+++ data/study/src/main/java/com/together/study/study/service/StudyMemberService.kt
@@ -0,0 +1,28 @@
+package com.together.study.study.service
+
+import com.together.study.remote.model.BaseResponse
+import com.together.study.study.dto.StudyMemberPlannerResponse
+import com.together.study.study.dto.StudyMemberProfileResponse
+import com.together.study.study.dto.StudyMemberTimeBlockResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface StudyMemberService {
+    @GET("studies/{studyId}/members/{userId}/profiles")
+    suspend fun getStudyMemberProfile(
+        @Path("studyId") studyId: Long,
+        @Path("userId") userId: Long,
+    ): BaseResponse<StudyMemberProfileResponse>
+
+    @GET("studies/{studyId}/members/{userId}/study-times")
+    suspend fun getStudyMemberTimeBlocks(
+        @Path("studyId") studyId: Long,
+        @Path("userId") userId: Long,
+    ): BaseResponse<StudyMemberTimeBlockResponse>
+
+    @GET("studies/{studyId}/members/{userId}/planners")
+    suspend fun getStudyMemberPlanner(
+        @Path("studyId") studyId: Long,
+        @Path("userId") userId: Long,
+    ): BaseResponse<StudyMemberPlannerResponse>
+}
