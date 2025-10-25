f07f7c5129531afeb56e8583be0abdaff46a39f2
chrin05 <rinring105@gmail.com>
2025-10-25 11:54:08 +0900

    [mod/#43] 정렬 enum클래스 내 request값 수정

--- core/common/src/main/java/com/together/study/common/type/study/StudySortingType.kt
+++ core/common/src/main/java/com/together/study/common/type/study/StudySortingType.kt
@@ -5,6 +5,6 @@ enum class StudySortingType(
     val request: String,
 ) {
     RECENT("최신순", "latest"),
-    MIN_COUNT("참여인원 적은 순", "most"),
-    MAX_COUNT("참여인원 많은 순", "least"),
+    MIN_COUNT("참여인원 적은 순", "least"),
+    MAX_COUNT("참여인원 많은 순", "most"),
 }
