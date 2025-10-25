74378fd2e01225b5accf3a46c8c2fbe8b8b2eb41
chrin05 <rinring105@gmail.com>
2025-10-25 23:15:18 +0900

    [ui/#47] 스터디 나가기 다이얼로그 추가

--- presentation/studysettings/src/main/java/com/together/study/studysettings/main/MemberSettingsScreen.kt
+++ presentation/studysettings/src/main/java/com/together/study/studysettings/main/MemberSettingsScreen.kt
@@ -6,15 +6,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.together.study.designsystem.R.drawable.ic_left_chevron
+import com.together.study.designsystem.component.dialog.TogedyBasicDialog
 import com.together.study.designsystem.component.topbar.TogedyTopBar
 import com.together.study.designsystem.theme.TogedyTheme
+import com.together.study.studysettings.component.SettingsSection
 
 @Composable
 fun MemberSettingsRoute(
@@ -22,12 +32,20 @@ fun MemberSettingsRoute(
     onMemberClick: () -> Unit,
     onReportClick: () -> Unit,
 ) {
+    var studyName = "햄부기"
+    var isExitDialogVisible by remember { mutableStateOf(false) }
+
     val memberEdit = listOf(
         Settings(title = "멤버 보기", onClick = onMemberClick),
     )
     val deleteEdit = listOf(
         Settings(title = "문제 신고하기", icon = null, onClick = onReportClick),
-        Settings(title = "스터디 나가기", icon = null, isTextRed = true, onClick = { /* dialog */ })
+        Settings(
+            title = "스터디 나가기",
+            icon = null,
+            isTextRed = true,
+            onClick = { isExitDialogVisible = true }
+        ),
     )
 
     Column(
@@ -52,6 +70,31 @@ fun MemberSettingsRoute(
         SettingsSection(
             sectionTitle = "스터디 삭제 설정",
             items = deleteEdit,
+        )
+    }
+
+    if (isExitDialogVisible) {
+        TogedyBasicDialog(
+            title = "스터디 나가기",
+            subTitle = {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = TogedyTheme.typography.body14b.toSpanStyle()) {
+                            append(
+                                studyName
+                            )
+                        }
+                        append(" 스터디를 나갈까요?")
+                    },
+                    style = TogedyTheme.typography.body14m,
+                    color = TogedyTheme.colors.gray900,
+                    textAlign = TextAlign.Center,
+                )
+            },
+            buttonText = "나가기",
+            buttonColor = TogedyTheme.colors.red,
+            onDismissRequest = { isExitDialogVisible = false },
+            onButtonClick = { /* 나가기 api */ }
         )
     }
 }
