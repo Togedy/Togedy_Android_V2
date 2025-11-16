2b00b484c9abf755f781944fc4f0ffa52c93c3f4
chrin05 <rinring105@gmail.com>
2025-11-16 21:33:03 +0900

    [ui/#57] 가입 다이얼로그에 비밀번호 입력란 추가

--- presentation/studydetail/src/main/java/com/together/study/studydetail/component/JoinStudyDialog.kt
+++ presentation/studydetail/src/main/java/com/together/study/studydetail/component/JoinStudyDialog.kt
@@ -1,5 +1,13 @@
 package com.together.study.studydetail.component
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,9 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.together.study.designsystem.component.dialog.TogedyBasicDialog
 import com.together.study.designsystem.theme.TogedyTheme
 
@@ -22,45 +32,114 @@ fun JoinStudyDialog(
     onDismissRequest: () -> Unit,
     onJoinStudyClick: () -> Unit,
 ) {
-    var input by remember { mutableStateOf("") }
+    val hasPassword = !password.isNullOrBlank()
+    var inputValue by remember { mutableStateOf("") }
     var showError by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
 
     TogedyBasicDialog(
         title = "스터디 가입",
         subTitle = {
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(
-                        TogedyTheme.typography.body14b.toSpanStyle()
-                            .copy(TogedyTheme.colors.gray900)
-                    ) {
-                        append(studyName)
-                    }
-                    withStyle(
-                        TogedyTheme.typography.body14m.toSpanStyle()
-                            .copy(TogedyTheme.colors.gray700)
-                    ) {
-                        append("\n스터디에 가입할까요?")
-                    }
-                },
-                textAlign = TextAlign.Center,
-            )
-            if (!password.isNullOrBlank()) {
-                //TODO : 비밀번호 입력
-                //오류 메시지 부분
+            if (hasPassword) {
+                Text(
+                    text = studyName,
+                    style = TogedyTheme.typography.body14b,
+                    color = TogedyTheme.colors.gray900,
+                )
+
+                Spacer(Modifier.height(10.dp))
+
+                PasswordTextField(
+                    inputValue = inputValue,
+                    onValueChange = { inputValue = it },
+                )
+
+                if (showError) {
+                    Spacer(Modifier.height(2.dp))
+
+                    Text(
+                        text = errorMessage,
+                        style = TogedyTheme.typography.chip10sb,
+                        color = TogedyTheme.colors.red,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+
+            } else {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            TogedyTheme.typography.body14b.toSpanStyle()
+                                .copy(TogedyTheme.colors.gray900)
+                        ) {
+                            append(studyName)
+                        }
+                        withStyle(
+                            TogedyTheme.typography.body14m.toSpanStyle()
+                                .copy(TogedyTheme.colors.gray700)
+                        ) {
+                            append("\n스터디에 가입할까요?")
+                        }
+                    },
+                    textAlign = TextAlign.Center,
+                )
             }
         },
         buttonText = "가입하기",
         onDismissRequest = onDismissRequest,
         onButtonClick = {
-            onJoinStudyClick()
-//            if (!hasPassword) onJoinStudyClick
-//            else {
-//                if (password == input) onJoinStudyClick
-//                else showError = true
-//            }
+            if (hasPassword && password != inputValue) {
+                showError = true
+                errorMessage =
+                    if (inputValue.toIntOrNull() == null) {
+                        "숫자 4자리 비밀번호를 입력해주세요."
+                    } else {
+                        "비밀번호가 올바르지 않습니다."
+                    }
+            } else {
+                showError = false
+                onJoinStudyClick()
+            }
         },
         modifier = modifier,
+    )
+}
+
+@Composable
+private fun PasswordTextField(
+    inputValue: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+) {
+    val borderColor =
+        if (inputValue.isEmpty()) TogedyTheme.colors.gray200
+        else TogedyTheme.colors.green
+
+    BasicTextField(
+        value = inputValue,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(4.dp)
+            )
+            .padding(10.dp),
+        textStyle = TogedyTheme.typography.body14m,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+        decorationBox = { innerTextField ->
+            if (inputValue.isEmpty()) {
+                Text(
+                    text = "비밀번호를 입력해주세요",
+                    style = TogedyTheme.typography.body14m,
+                    color = TogedyTheme.colors.gray500,
+                )
+            }
+
+            innerTextField()
+        }
     )
 }
 
@@ -70,7 +149,7 @@ private fun JoinStudyDialogPreview() {
     TogedyTheme {
         JoinStudyDialog(
             studyName = "햄버거파이터즈",
-            password = null,
+            password = "1234",
             modifier = Modifier,
             onDismissRequest = {},
             onJoinStudyClick = {},
