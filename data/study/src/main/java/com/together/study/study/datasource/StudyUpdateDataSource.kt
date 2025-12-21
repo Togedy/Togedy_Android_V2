8bbb0cd99bc4abf1de4f628dbd48c9e82b10a699
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-12-21 12:42:40 +0900

    [add/#65] 스터디 생성 data source 추가

---
+++ data/study/src/main/java/com/together/study/study/datasource/StudyUpdateDataSource.kt
@@ -0,0 +1,41 @@
+package com.together.study.study.datasource
+
+import android.content.Context
+import android.net.Uri
+import com.together.study.study.service.StudyUpdateService
+import com.together.study.util.ImageConverter
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import javax.inject.Inject
+
+class StudyUpdateDataSource @Inject constructor(
+    private val studyUpdateService: StudyUpdateService,
+    @ApplicationContext private val context: Context,
+) {
+    suspend fun createStudy(
+        challengeGoalTime: Int?,
+        studyName: String,
+        studyDescription: String?,
+        studyMemberLimit: Int,
+        studyTag: String,
+        studyPassword: String?,
+        studyImageUri: Uri?,
+    ) = studyUpdateService.createStudy(
+        challengeGoalTime = challengeGoalTime?.toString()
+            ?.toRequestBody("text/plain".toMediaType()),
+        studyName = studyName.toRequestBody("text/plain".toMediaType()),
+        studyDescription = studyDescription?.toRequestBody("text/plain".toMediaType()),
+        studyMemberLimit = studyMemberLimit.toString().toRequestBody("text/plain".toMediaType()),
+        studyTag = studyTag.toRequestBody("text/plain".toMediaType()),
+        studyPassword = studyPassword?.toRequestBody("text/plain".toMediaType()),
+        studyImage = studyImageUri?.let { uri ->
+            val file = ImageConverter.uriToFile(context, uri)
+            val requestFile = file.asRequestBody("image/*".toMediaType())
+            MultipartBody.Part.createFormData("studyImage", file.name, requestFile)
+        },
+    )
+}
+
