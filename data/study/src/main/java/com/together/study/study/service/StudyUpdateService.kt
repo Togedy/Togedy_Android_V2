4dfad3e82973e48bd9ff965a1c79a920e083f45e
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-12-21 15:46:19 +0900

    [feat/#65] 스터디 중복 검사 api service 추가

--- data/study/src/main/java/com/together/study/study/service/StudyUpdateService.kt
+++ data/study/src/main/java/com/together/study/study/service/StudyUpdateService.kt
@@ -1,11 +1,15 @@
 package com.together.study.study.service
 
+import com.together.study.remote.model.BaseResponse
+import com.together.study.study.dto.StudyDuplicateResponse
 import com.together.study.remote.model.EmptyDataResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface StudyUpdateService {
     @Multipart
@@ -19,5 +23,10 @@ interface StudyUpdateService {
         @Part("studyPassword") studyPassword: RequestBody?,
         @Part studyImage: MultipartBody.Part?,
     ): EmptyDataResponse
+
+    @GET("studies/duplicate")
+    suspend fun checkStudyNameDuplicate(
+        @Query("name") name: String,
+    ): BaseResponse<StudyDuplicateResponse>
 }
 
