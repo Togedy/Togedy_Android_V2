72907b3e769f3fb307c2f71c8fff0f873c38bb9b
Chaerin Kim <rinring105@gmail.com>
2026-02-05 23:48:29 +0900

    [mod/#82] plannerNavgraph 인자 수정

--- presentation/main/src/main/java/com/together/study/main/MainScreen.kt
+++ presentation/main/src/main/java/com/together/study/main/MainScreen.kt
@@ -181,7 +181,6 @@ private fun MainNavHost(
 
         plannerGraph(
             navigateToUp = navigator.navController::popBackStack,
-            navigateToSharePlanner = { /*navigator.navController::navigateToSharePlanner*/ },
             navController = navigator.navController,
             modifier = modifier,
         )
