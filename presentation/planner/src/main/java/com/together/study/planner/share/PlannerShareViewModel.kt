ddb8e994004791cf763a2c0b62a0bb10309a14e7
Chaerin Kim <rinring105@gmail.com>
2026-02-06 23:00:34 +0900

    [fix/#82] 코드리뷰 반영 - null 체크 로직 수정 - 전체선택 여부 확인 - uiState 업데이트 로직 수정

--- presentation/planner/src/main/java/com/together/study/planner/share/PlannerShareViewModel.kt
+++ presentation/planner/src/main/java/com/together/study/planner/share/PlannerShareViewModel.kt
@@ -11,6 +11,7 @@ import com.together.study.planner.share.state.PlannerShareUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,43 +31,47 @@ internal class PlannerShareViewModel @Inject constructor(
     val showTodo = _showTodo.asStateFlow()
 
     suspend fun getPlannerShareInfo() {
-        _uiState.value.plannerShareInfo = UiState.Success(
-            PlannerShareInfo(
-                date = "",
-                dDay = DDay(true, "수능", -100),
-                totalStudyTime = "00:00:00",
-                image = "",
-                plannerItemList = listOf(
-                    PlannerItem(
-                        subjectId = 1,
-                        subjectName = "수학",
-                        subjectColor = "SUBJECT_COLOR2",
-                        totalTaskCount = 5,
-                        checkedTaskCount = 3,
-                        taskList = listOf(
-                            TaskItem(1, "할 일1", false),
-                            TaskItem(2, "EBS 수학", true),
-                        )
-                    ),
-                    PlannerItem(
-                        subjectId = 2,
-                        subjectName = "수학",
-                        subjectColor = "SUBJECT_COLOR2",
-                        totalTaskCount = 5,
-                        checkedTaskCount = 3,
-                        taskList = listOf(
-                            TaskItem(1, "할 일1", false),
-                            TaskItem(2, "EBS 수학", true),
-                            TaskItem(1, "할 일1", false),
-                            TaskItem(2, "EBS 수학", true),
-                            TaskItem(1, "할 일1", false),
-                            TaskItem(2, "EBS 수학", true),
-                        )
-                    ),
-                ),
-                timeTableList = listOf(),
+        _uiState.update {
+            it.copy(
+                plannerShareInfo = UiState.Success(
+                    PlannerShareInfo(
+                        date = "",
+                        dDay = DDay(true, "수능", -100),
+                        totalStudyTime = "00:00:00",
+                        image = "",
+                        plannerItemList = listOf(
+                            PlannerItem(
+                                subjectId = 1,
+                                subjectName = "수학",
+                                subjectColor = "SUBJECT_COLOR2",
+                                totalTaskCount = 5,
+                                checkedTaskCount = 3,
+                                taskList = listOf(
+                                    TaskItem(1, "할 일1", false),
+                                    TaskItem(2, "EBS 수학", true),
+                                )
+                            ),
+                            PlannerItem(
+                                subjectId = 2,
+                                subjectName = "수학",
+                                subjectColor = "SUBJECT_COLOR2",
+                                totalTaskCount = 5,
+                                checkedTaskCount = 3,
+                                taskList = listOf(
+                                    TaskItem(1, "할 일1", false),
+                                    TaskItem(2, "EBS 수학", true),
+                                    TaskItem(1, "할 일1", false),
+                                    TaskItem(2, "EBS 수학", true),
+                                    TaskItem(1, "할 일1", false),
+                                    TaskItem(2, "EBS 수학", true),
+                                )
+                            ),
+                        ),
+                        timeTableList = listOf(),
+                    )
+                )
             )
-        )
+        }
 
         _subjects.value =
             (_uiState.value.plannerShareInfo as UiState.Success).data.plannerItemList.map {
@@ -76,14 +81,14 @@ internal class PlannerShareViewModel @Inject constructor(
                     it.subjectColor,
                 )
             }
-        _selectedSubjects.value = _subjects.value.map { it.subjectId.let { id -> id!! } }
+        _selectedSubjects.value = _subjects.value.mapNotNull { it.subjectId }
     }
 
     fun updateIsAllSelected() {
         _isAllSelected.value = !_isAllSelected.value
 
         if (_isAllSelected.value) _selectedSubjects.value =
-            subjects.value.map { it.subjectId.let { id -> id!! } }
+            subjects.value.mapNotNull { it.subjectId }
         else _selectedSubjects.value = emptyList()
     }
 
@@ -92,10 +97,9 @@ internal class PlannerShareViewModel @Inject constructor(
     }
 
     fun updateSelectedSubjects(new: Long) {
-        if (_selectedSubjects.value.contains(new)) {
-            _selectedSubjects.value -= new
-            if (_selectedSubjects.value.isEmpty()) updateIsAllSelected()
-        }
+        if (_selectedSubjects.value.contains(new)) _selectedSubjects.value -= new
         else _selectedSubjects.value += new
+
+        _isAllSelected.value = _selectedSubjects.value.size == subjects.value.size
     }
 }
--- presentation/planner/src/main/java/com/together/study/planner/share/state/PlannerShareUiState.kt
+++ presentation/planner/src/main/java/com/together/study/planner/share/state/PlannerShareUiState.kt
@@ -7,7 +7,7 @@ import com.together.study.planner.model.PlannerItem
 
 @Immutable
 data class PlannerShareUiState(
-    var plannerShareInfo: UiState<PlannerShareInfo> = UiState.Loading,
+    val plannerShareInfo: UiState<PlannerShareInfo> = UiState.Loading,
 )
 
 data class PlannerShareInfo(
