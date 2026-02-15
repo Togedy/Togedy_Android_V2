734b8ae8c4e917ae19cc9e7abfad19a95af16ccb
Chaerin Kim <rinring105@gmail.com>
2026-02-15 23:43:51 +0900

    [add/#86] timer 모듈 추가

---
+++ presentation/timer/build.gradle.kts
@@ -0,0 +1,25 @@
+import com.together.study.buildlogic.dsl.setNameSpace
+
+plugins {
+    id("com.together.study.buildlogic.convention.feature")
+    id("com.together.study.buildlogic.convention.compose")
+    id("com.together.study.buildlogic.primitive.hilt")
+}
+
+android {
+    setNameSpace("presentation.timer")
+}
+
+dependencies {
+
+    implementation(projects.core.common)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.util)
+    implementation(projects.domain.study)
+    implementation(projects.presentation.study)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.timber)
+}
