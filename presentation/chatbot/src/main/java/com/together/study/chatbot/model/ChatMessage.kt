4ca6f5285e15f98aad296325447b04064f778ad9
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-02-22 16:11:06 +0900

    [feat/#87] 채팅 아이템 데이터 필드 수정
    - 채팅 아이디 랜덤 uuid로 임의 설정
    - 글자 하나씩 출력 애니메이션을 위한 displaytext 필드 추가
    - 채팅 추가 시 애니메이션 여부 필드 추가

--- presentation/chatbot/src/main/java/com/together/study/chatbot/model/ChatMessage.kt
+++ presentation/chatbot/src/main/java/com/together/study/chatbot/model/ChatMessage.kt
@@ -1,7 +1,12 @@
 package com.together.study.chatbot.model
 
+import java.util.UUID
+
 data class ChatMessage(
+    val id: String = UUID.randomUUID().toString(),
     val message: String,
     val isMine: Boolean,
     val isLoading: Boolean = false,
+    val displayedText: String = message,
+    val isAnimating: Boolean = false,
 )
