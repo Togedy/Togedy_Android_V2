8e0bb0283017ada8e0c5486a058d6343a435710d
chrin105 <rinring105@gmail.com>
2026-03-11 17:42:34 +0900

    [add/#92] domain:support 모듈추가

---
+++ domain/support/src/main/java/com/together/study/support/Notice.kt
@@ -0,0 +1,7 @@
+package com.together.study.support
+
+data class Notice(
+    val noticeId: Long,
+    val noticeName: String,
+    val publishedAt: String,
+)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -42,6 +42,7 @@ include(
     ":domain:study",
     ":domain:planner",
     ":domain:search",
+    ":domain:support",
 )
 include(
     ":presentation:main",
