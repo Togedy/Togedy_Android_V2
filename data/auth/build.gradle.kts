be80beaa4dcf6a029ffe73cb237d7df0107f5de7
chrin105 <rinring105@gmail.com>
2026-03-11 11:41:58 +0900

    [mod/#52] dataStore에 토큰 저장 로직 추가

--- data/auth/build.gradle.kts
+++ data/auth/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(projects.core.util)
     implementation(projects.core.common)
 
+    implementation(projects.data.local)
     implementation(projects.data.remote)
     implementation(projects.domain.auth)
     implementation(libs.timber)
--- data/auth/src/main/java/com/together/study/auth/repositoryImpl/AuthRepositoryImpl.kt
+++ data/auth/src/main/java/com/together/study/auth/repositoryImpl/AuthRepositoryImpl.kt
@@ -4,14 +4,23 @@ import com.together.study.auth.datasource.AuthDataSource
 import com.together.study.auth.mapper.toDomain
 import com.together.study.auth.model.KakaoLogin
 import com.together.study.auth.repository.AuthRepository
+import com.together.study.local.TokenDataStore
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authDataSource: AuthDataSource,
+    private val tokenDataStore: TokenDataStore,
 ) : AuthRepository {
     override suspend fun loginWithKakao(accessToken: String): Result<KakaoLogin> =
         runCatching {
-            authDataSource.loginWithKakao(accessToken).toDomain()
+            val response = authDataSource.loginWithKakao(accessToken)
+
+            tokenDataStore.setTokens(
+                accessToken = response.jwtTokenInfo.accessToken,
+                refreshToken = response.jwtTokenInfo.refreshToken,
+            )
+
+            response.toDomain()
         }
 
 }
