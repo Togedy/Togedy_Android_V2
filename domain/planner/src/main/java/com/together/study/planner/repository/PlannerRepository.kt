0e695cf685d60dc48ea137649a5cbc0ec86e08c2
김채린 <chrin105@naver.com>
2026-03-18 17:32:26 +0900

    [mod/#94] data type 수정

--- domain/planner/src/main/java/com/together/study/planner/repository/PlannerRepository.kt
+++ domain/planner/src/main/java/com/together/study/planner/repository/PlannerRepository.kt
@@ -8,6 +8,6 @@ interface PlannerRepository {
     suspend fun postDailyPlannerImage(date: String): Result<Unit>
     suspend fun getDailyPlannerInfo(date: String): Result<DailyPlannerInfo>
     suspend fun getDailyStatistics(date: String): Result<DailyStatistics>
-    suspend fun getDailyTimetable(date: String): Result<TimeTable>
+    suspend fun getDailyTimetable(date: String): Result<List<TimeTable>>
     suspend fun getMonthlyHeatmap(month: String): Result<List<Int>>
 }
