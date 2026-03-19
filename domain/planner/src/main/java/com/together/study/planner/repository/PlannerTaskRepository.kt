331b88592ef1759559f385a6dc8571b69837b9e7
김채린 <chrin105@naver.com>
2026-03-19 16:08:36 +0900

    [feat/#98] repository 및 impl 구현

---
+++ domain/planner/src/main/java/com/together/study/planner/repository/PlannerTaskRepository.kt
@@ -0,0 +1,11 @@
+package com.together.study.planner.repository
+
+import com.together.study.planner.model.PlannerSubject
+import com.together.study.planner.model.TaskItem
+
+interface PlannerTaskRepository {
+    suspend fun getPlannerTaskList(date: String): Result<List<PlannerSubject>>
+    suspend fun updateTaskContent(task: TaskItem, subjectId: Long, date: String): Result<Int>
+    suspend fun deleteTask(taskId: Long): Result<Unit>
+    suspend fun updateTaskChecked(taskId: Long, isChecked: Boolean): Result<Unit>
+}
