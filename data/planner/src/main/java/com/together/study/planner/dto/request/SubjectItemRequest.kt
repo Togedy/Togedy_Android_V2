64b26c28479559402783f8613812cefa3dbe7226
Chaerin Kim <rinring105@gmail.com>
2026-03-24 20:14:41 +0900

    [feat/#95] dto 및 Service 구현

---
+++ data/planner/src/main/java/com/together/study/planner/dto/request/SubjectItemRequest.kt
@@ -0,0 +1,9 @@
+package com.together.study.planner.dto.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SubjectItemRequest(
+    val subjectName: String,
+    val subjectColor: String,
+)
