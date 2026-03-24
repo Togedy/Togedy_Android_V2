808fe18bb5b98bbaabca91716ecc7377562e6d3f
Chaerin Kim <rinring105@gmail.com>
2026-03-24 21:21:44 +0900

    [feat/#95] 과목 수정, 삭제 api 연결

--- presentation/planner/src/main/java/com/together/study/planner/subject/SubjectDetailViewModel.kt
+++ presentation/planner/src/main/java/com/together/study/planner/subject/SubjectDetailViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.together.study.common.state.UiState
 import com.together.study.planner.model.PlannerSubject
 import com.together.study.planner.model.SubjectItem
+import com.together.study.planner.usecase.DeleteSubjectUseCase
 import com.together.study.planner.usecase.GetSubjectsUseCase
+import com.together.study.planner.usecase.PatchSubjectUseCase
 import com.together.study.planner.usecase.PostSubjectUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +20,8 @@ import javax.inject.Inject
 class SubjectDetailViewModel @Inject constructor(
     private val getSubjectsUseCase: GetSubjectsUseCase,
     private val postSubjectUseCase: PostSubjectUseCase,
+    private val patchSubjectUseCase: PatchSubjectUseCase,
+    private val deleteSubjectUseCase: DeleteSubjectUseCase,
 ) : ViewModel() {
     private val _subjectState = MutableStateFlow<UiState<List<SubjectItem>>>(UiState.Loading)
     val subjectState = _subjectState.asStateFlow()
@@ -50,19 +54,45 @@ class SubjectDetailViewModel @Inject constructor(
     }
 
     fun updateSubject(new: SubjectItem) = viewModelScope.launch {
-        val updatedList = lastedSubjectItems.map { subject ->
-            if (subject.subjectId == new.subjectId) subject.copy(
-                subjectName = new.subjectName,
-                subjectColor = new.subjectColor,
-            )
-            else subject
-        }
-        updateState(UiState.Success(updatedList))
+        val origin = lastedSubjectItems
+            .find { it.subjectId == new.subjectId }
+            ?: return@launch
+
+        patchSubjectUseCase(
+            id = new.subjectId!!,
+            originName = origin.subjectName,
+            originColor = origin.subjectColor,
+            name = new.subjectName,
+            color = new.subjectColor
+        )
+            .onSuccess {
+                val updatedList = lastedSubjectItems.map { subject ->
+                    if (subject.subjectId == new.subjectId) {
+                        subject.copy(
+                            subjectName = new.subjectName,
+                            subjectColor = new.subjectColor,
+                        )
+                    } else subject
+                }
+
+                updateState(UiState.Success(updatedList))
+            }
+            .onFailure {
+                // toast
+            }
+
     }
 
     fun deleteSubject(id: Long) = viewModelScope.launch {
-        val updatedList = lastedSubjectItems.filter { it.subjectId != id }
-        updateState(UiState.Success(updatedList))
+        deleteSubjectUseCase(id)
+            .onSuccess {
+                val updatedList = lastedSubjectItems.filter { it.subjectId != id }
+                updateState(UiState.Success(updatedList))
+            }
+            .onFailure {
+                //toast
+            }
+
     }
 
 
