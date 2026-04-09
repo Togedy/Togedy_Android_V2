e01323dcd106018d400b8c2de51998b608e3353c
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-04-09 23:50:13 +0900

    [feat/#52] 로그아웃 viewmodel 연결 및 eventBus emit 추가

--- presentation/mypage/src/main/java/com/together/study/mypage/ui/account/AccountSettingsViewModel.kt
+++ presentation/mypage/src/main/java/com/together/study/mypage/ui/account/AccountSettingsViewModel.kt
@@ -2,21 +2,29 @@ package com.together.study.mypage.ui.account
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.together.study.auth.usecase.LogoutUseCase
 import com.together.study.common.state.UiState
 import com.together.study.mypage.state.AccountSettingsUiState
 import com.together.study.user.usecase.GetUserSettingInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountSettingsViewModel @Inject constructor(
     private val getUserSettingInfoUseCase: GetUserSettingInfoUseCase,
+    private val logoutUseCase: LogoutUseCase,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(AccountSettingsUiState())
     val uiState = _uiState.asStateFlow()
+
+    private val _logoutEvent = MutableSharedFlow<Unit>()
+    val logoutEvent = _logoutEvent.asSharedFlow()
 
     init {
         loadUserSettingInfo()
@@ -33,6 +41,12 @@ class AccountSettingsViewModel @Inject constructor(
     }
 
     fun logout() = viewModelScope.launch {
-        /* TODO API 연결 */
+        logoutUseCase()
+            .onSuccess {
+                _logoutEvent.emit(Unit)
+            }
+            .onFailure {
+                Timber.e(it, "로그아웃 실패")
+            }
     }
 }
