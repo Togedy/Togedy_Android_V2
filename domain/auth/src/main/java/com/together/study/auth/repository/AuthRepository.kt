a4c0ba3b9f6ee92aeb500b893cb4faea8903142e
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-04-09 23:49:10 +0900

    [feat/#52] 로그아웃 domain 모듈 로직 추가

--- domain/auth/src/main/java/com/together/study/auth/repository/AuthRepository.kt
+++ domain/auth/src/main/java/com/together/study/auth/repository/AuthRepository.kt
@@ -4,4 +4,5 @@ import com.together.study.auth.model.KakaoLogin
 
 interface AuthRepository {
     suspend fun postLoginKakao(accessToken: String) : Result<KakaoLogin>
+    suspend fun postLogout(): Result<Unit>
 }
