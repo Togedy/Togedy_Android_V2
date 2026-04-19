6f058d2152ce3a517fbcea584afaf22b96a00583
Chaerin Kim <rinring105@gmail.com>
2026-04-19 20:32:35 +0900

    [feat/#111] service 구현

---
+++ data/timer/src/main/java/com/together/study/timer/service/TimerService.kt
@@ -0,0 +1,26 @@
+package com.together.study.timer.service
+
+import com.together.study.remote.model.BaseResponse
+import com.together.study.timer.dto.RunningTimerResponse
+import com.together.study.timer.dto.SubjectTimerResponse
+import com.together.study.timer.dto.TimerResponse
+import com.together.study.timer.dto.TotalTimerResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface TimerService {
+    @GET("timers/total")
+    suspend fun getTotalStudyTimer(): BaseResponse<TotalTimerResponse>
+
+    @GET("timers/running")
+    suspend fun getRunningTimer(): BaseResponse<RunningTimerResponse>
+
+    @GET("timers/summary")
+    suspend fun getSummaryTimer(): BaseResponse<List<SubjectTimerResponse>>
+
+    @POST("timers/stop")
+    suspend fun stopTimer(timerId: Long): Result<TimerResponse>
+
+    @POST("timers/start")
+    suspend fun startTimer(subjectId: Long): Result<TimerResponse>
+}
