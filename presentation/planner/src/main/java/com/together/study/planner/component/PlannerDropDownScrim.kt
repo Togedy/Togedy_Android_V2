dc663bfddfc2bc022c1ec2f2384cc7f047383ae4
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-06-21 15:43:31 +0900

    [fix/#122] 플래너 드롭다운 아이템 터치영역 전체 넓이로 변경

--- presentation/planner/src/main/java/com/together/study/planner/component/PlannerDropDownScrim.kt
+++ presentation/planner/src/main/java/com/together/study/planner/component/PlannerDropDownScrim.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -108,6 +109,7 @@ fun PlannerDropDownScrimItem(
 ) {
     Row(
         modifier = modifier
+            .fillMaxWidth()
             .background(color = TogedyTheme.colors.white)
             .clickable(onClick = onClick)
             .padding(horizontal = 16.dp, vertical = 8.dp),
